ea5aba14a7914445ff3067ba9fefc38e69f6c549
Michael Peng <michael@mcmoo.org>
2019-10-12 13:25:30 -0400

    Set up website for Flask

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/historicalData')
+def historical_data():
+    return render_template('historicalData.html')
+
+@app.route('/liveData')
+def live_data():
+    return render_template('liveData.html')
+
+@app.route('/plan')
+def planner():
+    return render_template('plan.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
   app.run()
